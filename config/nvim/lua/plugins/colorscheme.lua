659bf1a82f736040965fed725f3967405d92a015
Ben Stephens <BKStephens@outlook.com>
2026-09-02 18:49:53 -0700

    Enable transparent mode for gruvbox in Neovim

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -5,11 +5,14 @@ return {
     priority = 1000,
     config = function()
       require("gruvbox").setup({
+        transparent_mode = true,
         contrast = "hard",
         palette_overrides = {},
         overrides = {
           SignColumn = { bg = "NONE" },
-          VertSplit = { bg = "#32302f", fg = "#504945" },
+          VertSplit = { bg = "NONE", fg = "#504945" },
+          NormalFloat = { bg = "NONE" },
+          FloatBorder = { bg = "NONE" },
         },
       })
       vim.cmd.colorscheme("gruvbox")
